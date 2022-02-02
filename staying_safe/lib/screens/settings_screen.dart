70ad73d0b9360e28a54704925d0e5ae98f9e228a
mohamed.merzais <mohamed.merzais@gmail.com>
2022-02-02 13:25:41 +0000

    settings page fixed

--- staying_safe/lib/screens/settings_screen.dart
+++ staying_safe/lib/screens/settings_screen.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:staying_safe/screens/Map_screen.dart';
 import 'package:staying_safe/screens/auth_screen.dart';
 import 'package:staying_safe/screens/home_screen.dart';
-import 'dart:async';
-import 'package:url_launcher/link.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class setting extends StatefulWidget {
   const setting({Key? key}) : super(key: key);
@@ -17,6 +14,47 @@ class setting extends StatefulWidget {
 class _settingState extends State<setting> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        automaticallyImplyLeading: false, //remove backbutton
+        centerTitle: true,
+        actions: [
+          PopupMenuButton<int>(
+              icon: const Icon(Icons.menu),
+              onSelected: (item) => onSelected(context, item),
+              itemBuilder: (context) => [
+                    const PopupMenuItem<int>(
+                      value: 0,
+                      child: Text('Main Menu'),
+                    ),
+                    const PopupMenuItem<int>(
+                      value: 1,
+                      child: Text('Log Out'),
+                    ),
+                  ])
+        ],
+      ),
+    );
+  }
+
+  void onSelected(BuildContext context, int item) {
+    switch (item) {
+      case 0:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Homescreen()),
+        );
+        break;
+      case 1:
+        () async {
+          await FirebaseAuth.instance.signOut();
+        };
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const AuthApp()),
+        );
+        break;
+    }
   }
 }
